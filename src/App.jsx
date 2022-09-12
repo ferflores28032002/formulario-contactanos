025a48a3e4e0490ca962384845aa2731553dd238
fernando flores <martinjanns2000@gmail.com>
2022-09-11 18:30:17 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import styles from "./App.module.css";
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
+import imagen from "./imagenes/gato.jpeg";
+import { useForm } from "react-hook-form";
+
+export const App = () => {
+  const [message, setmessage] = useState(0);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const enviarCorreo = (data) => {
+    emailjs
+      .send(
+        "service_7z6vxnq", //servicio del correo
+        "template_0us4xq7", //Plantilla del correo
+        data,
+        "SVvS_0wxxAsmWl7uC" //Api_key
+      )
+      .then((response) => {
+        setmessage(response.status);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className={styles.contenedor}>
+      <div
+        className="card mb-3 shadow-lg border-2 border"
+        style={{ maxWidth: "900px" }}
+      >
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={imagen} className="img-fluid rounded-start h-100" />
+          </div>
+
+          <div className="col-md-8 px-4">
+            <div className="card-body">
+              <h5 className="card-title mb-4">Enviar Mensaje</h5>
+              <form onSubmit={handleSubmit(enviarCorreo)}>
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Nombres"
+                    {...register("name", {
+                      required: true,
+                      minLength: 2,
+                    })}
+                  />
+                  {errors.name?.type === "required" && (
+                    <p className="card-text mt-1">El nombre es requerido</p>
+                  )}
+                  {errors.name?.type === "minLength" && (
+                    <p className="card-text mt-1">
+                      El nombre debe tener más de 2 carácteres
+                    </p>
+                  )}
+                  <label className="form-label">Nombres</label>
+                </div>
+                <div className="form-floating mt-3">
+                  <input
+                    type="number"
+                    className="form-control"
+                    placeholder="Teléfono"
+                    {...register("telefono", {
+                      required: true,
+                      minLength: 8,
+                    })}
+                  />
+                  {errors.telefono?.type === "required" && (
+                    <p className="card-text mt-1">El telefono es requerido</p>
+                  )}
+                  {errors.telefono?.type === "minLength" && (
+                    <p className="card-text mt-1">
+                      El teléfono debe tener más de 8 números
+                    </p>
+                  )}
+                  <label className="form-label">Teléfono</label>
+                </div>
+                <div className="form-floating mt-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    placeholder="mensaje@mail.com"
+                    {...register("email", {
+                      pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i,
+                      required: true,
+                    })}
+                  />
+                  {errors.email?.type === "pattern" && (
+                    <p className="card-text mt-1">
+                      El formato de email es incorrecto
+                    </p>
+                  )}
+                  {errors.email?.type === "required" && (
+                    <p className="card-text mt-1">El email es requerido</p>
+                  )}
+                  <label className="form-label">mensaje@mail.com</label>
+                </div>
+                <div className="form-floating mt-3">
+                  <textarea
+                    className="form-control"
+                    placeholder="Mensaje"
+                    style={{ height: "100px" }}
+                    {...register("mensaje", {
+                      required: true,
+                      minLength: 10,
+                    })}
+                  ></textarea>
+                  {errors.name?.type === "required" && (
+                    <p className="card-text mt-1">El Mensaje es requerido</p>
+                  )}
+                  {errors.mensaje?.type === "minLength" && (
+                    <p className="card-text mt-1">
+                      El Mensaje debe tener más de 10 carácteres
+                    </p>
+                  )}
+                  <label>Mensajes</label>
+                </div>
+                <div className="d-flex justify-content-end my-3">
+                  <button type="submit" className="btn btn-dark">
+                    Enviar Mensaje
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
